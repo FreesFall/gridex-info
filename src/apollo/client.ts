0b08c823b5270cdc6e187a2b566e1da307bca7fb
freelance <freelance@gamil.com>
2024-05-14 21:29:13 +0800

    fix client

--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -21,8 +21,8 @@ export const blockClient = new ApolloClient({
 })
 
 export const client = new ApolloClient({
-  uri: 'https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v3',
-  // uri: 'http://173.208.142.67:8000/subgraphs/name/bentokenfinance/bch-gridexinfo/graphql?',
+  // uri: 'https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v3',
+  uri: 'http://173.208.142.67:8000/subgraphs/name/bentokenfinance/bch-gridexinfo',
   cache: new InMemoryCache({
     typePolicies: {
       Token: {
